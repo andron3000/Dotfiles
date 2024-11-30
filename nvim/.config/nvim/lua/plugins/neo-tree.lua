22639a71ef906ff352fafcbf7a92622772d9a693
andrii.chekhovych <andrii.chekhovych@atlaslive.tech>
2024-11-30 19:25:42 +0200

    fixed dotfiles

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,7 +6,21 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-	config = function()
+
+  opts = {
+    filesystem = {
+      filtered_items = {
+	      visible = true,
+	      show_hidden_count = true,
+	      hide_dotfiles = false,
+	      hide_gitignored = true,
+	      never_show = {},
+      },
+    },
+  },
+
+	config = function(_, opts)
+    require("neo-tree").setup(opts)
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal toggle left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal toggle float<CR>", {})
 	end,
