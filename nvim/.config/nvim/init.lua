a95f522928cf0517060df9df68b560ae9fa41c3a
Andriy Chekhovych <chekhovych1995@gmail.com>
2025-02-05 19:20:28 +0200

    fixed dotfiles

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,3 +13,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
+
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,8 +1,7 @@
-
 vim.cmd("set shiftwidth=2")
 
 vim.g.mapleader = " "
-vim.g.loaded_netrw       = 1
+vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.opt.swapfile = false
@@ -10,12 +9,12 @@ vim.opt.guifont = "JetBrains Mono NL Font:h16"
 vim.opt.statusline = "%f %y %m %= %l,%c %p%%"
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- NeoTree
-vim.keymap.set("n", '<C-n>', ':Neotree filesystem reveal toggle left<CR>')
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal toggle left<CR>")
 
 vim.wo.number = true
